8ed7e8ce8e7b0e4a5b1a876cb259fee820321c24
Su Canhui <schlsj@163.com>
2017-01-10 17:28:42 +0800

    Just Off Duty

---
+++ SymbolAnalysis/Symbol.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolAnalysis
+{
+    //public class Gate
+    public enum SymbolResult
+    {
+        NameNull,
+        AddNull,
+        AddError,
+        AddOut,
+
+    }
+
+    public class Symbol
+    {
+        public string Name { get;  }
+        public string Address { get; }
+        public string Type { get; set; }
+        public string Comment { get;  }
+
+        public Symbol(string Name, string Address, string Type, string Comment)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Type = Type;
+            this.Comment = Comment;
+        }
+
+        public bool isValidate()
+        {
+            //if(string.IsNullOrEmpty(Name))
+            return false;
+        }
+    }
+}
